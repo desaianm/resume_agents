b01aba09a6e80858627cfdbf0fbc4757286921db
Anmol Desai <46572436+desaianm@users.noreply.github.com>
2024-03-22 00:26:41 -0400

    Update main.py
    small updates

--- main.py
+++ main.py
@@ -4,15 +4,9 @@ from tasks import Tasks
 from agents import Agents
 from dotenv import load_dotenv
 import pandas as pd
-from langchain_community.document_loaders import CSVLoader
-from langchain.document_loaders.csv_loader import CSVLoader
-from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 import json
 
 #os.environ['KMP_DUPLICATE_LIB_OK']='True'
-
 load_dotenv()
 
 tasks = Tasks()
@@ -27,18 +21,26 @@ parser_agent = agents.parser_agent()
 expert_agent = agents.expert_agent()
 writer_agent = agents.writer_agent()
 find_agent = agents.find_jobs()
+query_agent = agents.query_agent()
+search_agent = agents.search_agent()
 
 # Define Tasks for each agent
 review = tasks.extract_info(parser_agent,resume_temp)
-#report = tasks.create_analysis(expert_agent)
+report = tasks.create_analysis(expert_agent)
 job_list = tasks.job_list(find_agent)
 jobs_find = tasks.find_jobs(find_agent)
+create_query = tasks.create_query(query_agent)
+find_job_id = tasks.find_job_id(search_agent)
 
+# create query and find_job_id
 # Instantiate the crew with a sequential process
 crew = Crew(
-        agents=[parser_agent,find_agent],
+        agents=[parser_agent,find_agent, expert_agent],
         tasks=[
-            review, job_list, jobs_find
+            review,
+            report,
+            job_list,
+            jobs_find
         ]
 )
 
