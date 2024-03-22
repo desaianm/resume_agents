46a3c42e49e535827249bf0e29d11f6363066417
Anmol Desai <46572436+desaianm@users.noreply.github.com>
2024-03-22 00:27:52 -0400

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 from textwrap import dedent
 from crewai import Task
+from tools import SearchTool
 
 class Tasks():
 		def extract_info(self, agent, profile_temp):
@@ -14,30 +15,49 @@ class Tasks():
 		def create_analysis(self,agent):
 			return Task(
 						description=dedent(f"""\
-							Create a report of analysis for finding jobs by extracting keywords from relevant skills, experience and projects from resume	"""),
+							Create a report of analysis for finding jobs by extracting keywords from each section in resume json file	"""),
 						expected_output=dedent("""\
-							relevant keywords from resume in string format to find jobs"""),
-						agent=agent,
-						allow_delegation = False
+							 keywords from resume in string format to find jobs"""),
+						agent=agent
 				)
 
 		def job_list(self, agent):
 				return Task(
 						description=dedent(f"""\
-								Analyze the keywords about the ideal candidate have and match with similar jobs in  internships"""),
+								Analyze the keywords about the ideal candidate have and find job titles close to it. it doesn't have to be exact match.  """),
 						expected_output=dedent("""\
-								List of Intern job titles for relevant jobs"""),
+								List of  job titles for relevant jobs"""),
 						agent=agent
 				)
 		
 		def find_jobs(self,agent):
 			return Task(
 						description=dedent(f"""\
-								From the given list of job title and find job_id and company names """),
+								From the given list of job title, find jobs with company names in internships csv. Keep looking untill 5 found.make sure to include company name and job title in the output file.  """),
 						expected_output=dedent("""\
-								List with job titles and Company Name  found in  internships csv file"""),
-						agent=agent
+								List of job titles with Company Name in json format"""),
+						agent=agent,
+						output_file='jobs.json'
+						
 				)
+		def create_query(self,agent):
+			return Task(
+						description=dedent(f"""\
+								create a string with job title to find job id in internships csv and put it in search tool."""),
+						expected_output=dedent("""\
+								replace job_title in this {"query": "job_title"} with job title from job list json file."""),
+						agent=agent
+						
+				)
+		def find_job_id(self,agent):
+			return Task(
+						description=dedent(f"""\
+								input 'AI Research intern' into search tool to find job id from internships csv file."""),
+						expected_output=dedent("""\
+								Job id of the job title from internships csv file"""),
+						agent =  agent
+			)
+		
 		
 		
 
